8170d22dbac82b4b5701f0a5d9686ade6fc2fb06
xuanhoathuthuc <MingXiang2000MX@gmail.com>
2020-12-15 21:06:32 +0700

    index

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ close_footer.onclick = function () {
   modal.style.display = "none";
 }
 order.onclick = function () {
-  alert("Cảm ơn bạn đã thanh toán đơn hàng")
+	alert('Bạn đã thanh toán thành công!');
 }
 window.onclick = function (event) {
   if (event.target == modal) {
@@ -151,14 +151,35 @@ function addItemToCart(title, price, img) {
 	  updatecart()
 	})
 }  
-$('button#search-btn').click(function(){
-	var sText = $('input#search-text').val().toLowerCase();
-	
-	var x, i;
-	x = document.getElementsByClassName("food");
-	for (i = 0; i < x.length; i++) {
-		foodRemoveClass(x[i], "show");
-		if (x[i].innerHTML.toLowerCase().indexOf(sText) > -1) foodAddClass(x[i], "show");
-	}
-})
+// lấy thẻ input
+var input = document.getElementById("myInput");
+// định nghĩa hàm xử lý myFunction
+function myFunction() {
+	var filter, ul, li, a, i;
+	// lấy giá trị người dùng nhập
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("myUL");
+	li = ul.getElementsByTagName("li");
+	// Nếu filter không có giá trị thị ẩn phần kết quare\
+	if (!filter) {
+	  ul.style.display = "none";
+	}else{
+	  // lặp qua tất cả các thẻ li chứa kết quả
+	  for (i = 0; i < li.length; i++) {
+		  // lấy thẻ a trong các thẻ li
+		  a = li[i].getElementsByTagName("a")[0];
+		  // kiểm tra giá trị nhập có tôn tại trong nội dung thẻ a
+		  if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+			//nếu có hiển thị phàn tử ul và các thẻ li đó
+			  ul.style.display = "block";
+			  li[i].style.display = "";
+		  } else {
+			// nếu không ẩn các thẻ li
+			  li[i].style.display = "none";
 
+		  }
+	  }
+	}
+}
+//gán sự kiện cho thẻ input
+input.addEventListener("keyup", myFunction);
